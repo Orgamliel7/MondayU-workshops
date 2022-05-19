b90070a3dd5d531ed6d654a79954527390d503da
Orgamliel7 <57177424+Orgamliel7@users.noreply.github.com>
2022-05-19 11:09:45 +0300

    Update Dom_Manipulation_links&titles.js
    Adding of Vlad solution :)

--- Workshop 3 - Dom manipulation - extracting all links&titles from website/Dom_Manipulation_links&titles.js
+++ Workshop 3 - Dom manipulation - extracting all links&titles from website/Dom_Manipulation_links&titles.js
@@ -23,8 +23,51 @@ make_table()
 
 // Monday solution
 
-Array.prototype.map.call(document.querySelectorAll('.cd--card'), (el => {
+function getArticles() {
+  return Array.prototype.map.call(document.querySelectorAll(".cd--card"), el => {
     const text = el.innerText;
-const link = el.querySelector('a')?.href;
-return {text, link};
-}))
+    const link = el.querySelector("a")?.href;
+    return { text, link };
+  });
+}
+
+function clearBody() {
+  document.body.innerHTML = "";
+}
+
+function addLinks(articles) {
+  const ul = document.createElement("ul");
+  document.body.appendChild(ul);
+
+  articles.forEach(article => {
+    const a = document.createElement("a");
+    a.innerText = article.text;
+    a.href = article.link;
+
+    const li = document.createElement("li");
+    li.appendChild(a);
+
+    ul.appendChild(li);
+  });
+}
+
+function addStyle() {
+  // Change the colors of all "a" elements. Generally speaking, this isn't a good practice, but this isn't our focus right now.
+  const css = `
+  a:hover {
+    color: purple;
+  }
+
+  a:visited {
+    color: gray;
+  }
+  `;
+  const style = document.createElement("style");
+  style.appendChild(document.createTextNode(css));
+  document.head.appendChild(style);
+}
+
+const articles = getArticles();
+clearBody();
+addLinks(articles);
+addStyle();
